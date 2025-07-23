45b0d5081e262bd7d34c96abebda175bb1e858c6
Claudio G. Ferraz <contact@claudioferraz.com>
2025-07-23 16:19:34 -0300

    Change colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      vim.o.background = "dark"
       vim.cmd.colorscheme "gruvbox"
       -- vim.cmd [[
       --   highlight Normal guibg=none
