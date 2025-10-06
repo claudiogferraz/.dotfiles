84bfb1218b6a94ef670b7ffebfdb3cfeabacd19b
Claudio G. Ferraz <contact@claudioferraz.com>
2025-10-06 19:53:16 -0300

    Update mason-lspconfig

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,9 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
+    "nyoom-engineering/oxocarbon.nvim",
     config = function()
       vim.o.background = "dark"
-      vim.cmd.colorscheme "gruvbox"
-      -- vim.cmd [[
-      --   highlight Normal guibg=none
-      --   highlight NonText guibg=none
-      --   highlight Normal ctermbg=none
-      --   highlight NonText ctermbg=none
-      -- ]]
+      vim.cmd.colorscheme "oxocarbon"
     end,
-  },
+  }
 }
--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,38 @@
 return {
   "mason-org/mason-lspconfig.nvim",
-  opts = {},
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
-    "neovim/nvim-lspconfig",
-  }
+    "neovim/nvim-lspconfig"
+  },
+  opts = {
+    ensure_installed = {
+      "lua_ls",
+      "vtsls",
+      "cssls",
+      "tailwindcss",
+      "rust_analyzer",
+      "emmet_language_server",
+      "dockerls",
+    },
+  },
+  config = function()
+    local on_attach = function(client, bufnr)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
+    end
+
+    vim.lsp.config('lua_ls', {
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'require' }, -- Example custom setting
+          },
+        },
+      },
+    })
+
+    vim.lsp.config('*', {
+      on_attach = on_attach, -- Attach your keymaps/functions to ALL LSPs
+    })
+  end
 }
