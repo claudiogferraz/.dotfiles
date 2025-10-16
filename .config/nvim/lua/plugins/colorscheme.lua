475c271d82bd64ae82a796a0bec0c42eb9b6b7dc
Claudio G. Ferraz <contact@claudioferraz.com>
2025-10-16 08:02:26 -0300

    Update theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,7 @@
 return {
-  "Shatur/neovim-ayu",
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
   config = function()
-    require("ayu").setup({
-      overrides = {
-        Normal = { bg = "None" },
-        NormalFloat = { bg = "none" },
-        ColorColumn = { bg = "None" },
-        SignColumn = { bg = "None" },
-        Folded = { bg = "None" },
-        FoldColumn = { bg = "None" },
-        CursorLine = { bg = "None" },
-        CursorColumn = { bg = "None" },
-        VertSplit = { bg = "None" },
-      }
-    })
-    vim.cmd.colorscheme "ayu"
+    vim.cmd.colorscheme "gruvbox"
   end,
 }
