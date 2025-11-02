fa5e1bbeca22591b574332c3a045ea98f6c612b8
Claudio G. Ferraz <contact@claudioferraz.com>
2025-11-02 14:42:27 -0300

    Update plugins

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -62,9 +62,9 @@ return {
           inactive = 'ColorColumn',
         },
         symbols = {
-          modified = '', -- Text to show when the buffer is modified
-          alternate_file = '󱦙 ', -- Text to show to identify the alternate file
-          directory = '', -- Text to show when the buffer is a directory
+          modified = '',
+          alternate_file = '󱦙 ',
+          directory = '',
         },
       } },
       lualine_b = {},
