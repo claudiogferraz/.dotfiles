ed20e2ec2ba94a1a713496f42719503ecab2f10f
Claudio G. Ferraz <contact@claudioferraz.com>
2026-01-06 15:14:15 -0300

    Update plugins and add ensure_installed to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,29 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate"
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "rust",
+      "go",
+      "typescript",
+      "javascript",
+      "html",
+      "css",
+      "json",
+      "yaml",
+      "markdown",
+      "bash",
+      "python",
+      "env"
+    },
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true
+    },
+    auto_install = true,
+  }
 }
