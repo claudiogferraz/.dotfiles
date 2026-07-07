38b94ece2f4d6e3af739247402027005dc3b6c4c
Claudio G. Ferraz <contact@claudioferraz.com>
2026-07-07 14:08:13 -0300

    Fix mason-lspconfig config

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,19 +1,23 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
-    "neovim/nvim-lspconfig"
-  },
-  opts = {
-    ensure_installed = {
-      "lua_ls",
-      "vtsls",
-      "cssls",
-      "tailwindcss",
-      "rust_analyzer",
-      "emmet_language_server",
-      "dockerls",
-      "markdown_oxide",
-    },
-  },
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
+	opts = {
+		ensure_installed = {
+			"lua_ls",
+			"vtsls",
+			"cssls",
+			"tailwindcss",
+			"rust_analyzer",
+			"emmet_language_server",
+			"dockerls",
+			"markdown_oxide",
+			"eslint",
+			"jsonls",
+			"yamlls",
+			"html",
+		},
+	},
 }
